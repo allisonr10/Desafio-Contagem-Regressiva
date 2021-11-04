43552e5704528246d3d9e26fd9880b6b4fe6ea18
Allison Rodrigues <allison.ifg@gmail.com>
2021-11-04 10:48:08 -0300

    Finalizado
    Atividade finalizada

--- js/savethedate.js
+++ js/savethedate.js
@@ -14,19 +14,41 @@
     }
 
     const dataEvento = converterData(dataDom)
-    console.log(dataEvento)
-
+    
     //pegando data atual
 
     const dataAtual = new Date()
-
+    console.log(dataAtual)
+    console.log(dataEvento)
     //fazendo diferença entre as datas
 
-    const diferença = Math.abs(dataAtual.getTime() - dataEvento.getTime())
-    console.log(diferença)
+    let restante = dataEvento.getTime() - dataAtual.getTime()
 
-    const dias = Math.ceil(diferença / (1000 * 60 * 60 * 24))
-    console.log(dias)
-    const diferençaAnos = Math.round(dias/365)
-    console.log(diferençaAnos)
+    const umMinuto = 60 * 1000
+    const umaHora = 60 * umMinuto
+    const umDia = 24 * umaHora
+
+    const diasFaltam = parseInt(restante/umDia)
+    restante = restante - diasFaltam * umDia
+
+    const horasFaltam = parseInt(restante/umaHora)
+    restante = restante - horasFaltam * umaHora
+
+    const minutosFaltam = parseInt(restante/umMinuto)
+    restante = restante - minutosFaltam * umMinuto
+
+    const segundosRestam = parseInt(restante/1000)
+    restante = restante - segundosRestam * 1000
+
+    console.log( diasFaltam, horasFaltam, minutosFaltam, segundosRestam)
+
+    //função para adicionar os valores direto na página
+
+function adicionarValores(d,h,m,s){
+        const p = document.createElement('p')
+        p.textContent = `Contagem regressiva: Faltam ${d} dias, ${h} horas, ${m} minutos, ${s} segundos,`
+        document.querySelector('.hero-content').appendChild(p)
+    }
+    adicionarValores(diasFaltam, horasFaltam, minutosFaltam, segundosRestam)
+
 })()
